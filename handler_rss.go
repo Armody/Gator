0a76d5aa24c8b8692a70190c1452709da8d980b7
Armody <Rmdyzie@gmail.com>
2025-03-03 15:17:42 +0300

    2

--- handler_rss.go
+++ handler_rss.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/Armody/Gator/internal/RSS"
+	"github.com/Armody/Gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAgg(s *state, c command) error {
@@ -14,6 +17,35 @@ func handlerAgg(s *state, c command) error {
 		return fmt.Errorf("couldn't fetch feed: %w", err)
 	}
 
+	fmt.Println(feed)
+	return nil
+}
+
+func handlerAddFeed(s *state, c command) error {
+	if len(c.args) != 2 {
+		return fmt.Errorf("usage: addfeed <name> <url>")
+	}
+
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("no user logged in")
+	}
+	name := c.args[0]
+	url := c.args[1]
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("error creating feed: %w", err)
+	}
+
+	fmt.Println("Feed created successfully:")
 	fmt.Println(feed)
 	return nil
 }
--- handler_user.go
+++ handler_user.go
@@ -32,7 +32,7 @@ func handlerLogin(s *state, cmd command) error {
 
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
-		return fmt.Errorf("expected a username. Usage: gator login <username>")
+		return fmt.Errorf("expected a username. Usage: gator register <username>")
 	}
 
 	userName := cmd.args[0]
