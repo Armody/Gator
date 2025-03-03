99e6fbaf529b0953d507fd7816eb6a1abeba3451
Armody <Rmdyzie@gmail.com>
2025-03-03 12:42:43 +0300

    integrated db and added register

--- handler_user.go
+++ handler_user.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"time"
+
+	"github.com/Armody/Gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -12,10 +17,40 @@ func handlerLogin(s *state, cmd command) error {
 
 	userName := cmd.args[0]
 
-	err := s.cfg.SetUser(userName)
+	_, err := s.db.GetUser(context.Background(), userName)
+	if err != nil {
+		return errors.New("user doesn't exist")
+	}
+
+	err = s.cfg.SetUser(userName)
 	if err != nil {
 		return err
 	}
 	fmt.Println("User has been set to", userName)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return errors.New("expected a username. Usage: gator login <username>")
+	}
+
+	userName := cmd.args[0]
+
+	newUser, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      userName,
+	})
+	if err != nil {
+		return errors.New("user with that username already exists")
+	}
+
+	s.cfg.SetUser(userName)
+
+	fmt.Println("user", userName, "was created")
+	fmt.Println(newUser)
+
 	return nil
 }
--- main.go
+++ main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/Armody/Gator/internal/config"
+	"github.com/Armody/Gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
+const dbURL = "postgres://postgres:1234@localhost:5432/gator"
+
 func main() {
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	programState := &state{&cfg}
+	programState := &state{dbQueries, &cfg}
 
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	args := os.Args
 	if len(args) < 3 {
